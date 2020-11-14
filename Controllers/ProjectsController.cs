233cd4163d47e2893f9a149f2f82c15c5f3e813b
Petreyus <54726563+petreyus@users.noreply.github.com>
2020-11-14 15:41:51 -0600

    1st commit test inside folder

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+  //  [Authorize]
+    public class ProjectsController : Controller
+    {
+       
+        public ActionResult SpaceStationLocationRender() 
+        {
+            return View();
+        }
+
+
+
+
+    }
+
+}
